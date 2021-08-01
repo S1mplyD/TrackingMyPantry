0578a2a2268a30c1501f0353f9c24dd907681a17
S1mplyD <lucabennati2k16@gmail.com>
2021-08-01 10:48:46 +0200

    Created activity for Barcode scanning

---
+++ app/src/main/java/com/example/trackingmypantry/BarcodeScanner.kt
@@ -0,0 +1,11 @@
+package com.example.trackingmypantry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BarcodeScanner : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_barcode_scanner)
+    }
+}
