38312639892c30f800313c3654c01655c0240538
S1mplyD <lucabennati2k16@gmail.com>
2021-08-01 10:47:18 +0200

    Moved GET PRODUCTS function

--- app/src/main/java/com/example/trackingmypantry/HomePage.kt
+++ app/src/main/java/com/example/trackingmypantry/HomePage.kt
@@ -12,31 +12,14 @@ class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+        val accessToken = intent.getStringExtra("accessToken").toString()
         //val btn = findViewById<Button>(R.id.localproductsButton)
         //btn.setOnClickListener { val i = Intent(this, ) }
         val getProducts: Button = findViewById(R.id.getproductsButton)
         getProducts.setOnClickListener {
-            //TODO: create another activity to insert barcode or to scan it via camera
-            val apiUrl = "https://lam21.modron.network/products?barcode="
-            val barcode = "0000000000000"
-            val accessToken = intent.getStringExtra("accessToken").toString()
-            println(accessToken)
-            val client = OkHttpClient()
-            val request = Request.Builder()
-                    .addHeader("Authorization", "Bearer $accessToken")
-                    .url(apiUrl + barcode)
-                    .build()
-            client.newCall(request).enqueue(object : Callback {
-                override fun onFailure(call: Call, e: IOException) {
-                    Log.d("Error","failed to execute")
-                }
-
-                override fun onResponse(call: Call, response: Response) {
-                    val responseBody = response.body?.string()
-                    println(responseBody)
-                }
-
-            })
+            val i = Intent(this,GetProducts::class.java)
+            i.putExtra("accessToken",accessToken)
+            startActivity(i)
         }
     }
 }
