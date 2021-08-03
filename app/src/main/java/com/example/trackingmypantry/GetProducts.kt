962f857f844f1d1e1139e9864c7696110e373690
S1mplyD <lucabennati2k16@gmail.com>
2021-08-03 10:59:09 +0200

    Moved getProducts function into HTTPcalls class

--- app/src/main/java/com/example/trackingmypantry/GetProducts.kt
+++ app/src/main/java/com/example/trackingmypantry/GetProducts.kt
@@ -4,6 +4,7 @@ import android.content.ClipDescription
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -11,61 +12,32 @@ import android.widget.Toast
 import com.google.gson.GsonBuilder
 import okhttp3.*
 import java.io.IOException
+import com.example.trackingmypantry.HTTPcalls
+import com.example.trackingmypantry.LoginPage.Companion.accessToken
 
 class GetProducts : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_products)
-        val accessToken = intent.getStringExtra("accessToken").toString()
         val barcodeText: EditText = findViewById(R.id.barcode)
-        val barcode = barcodeText.text
+        val barcode = barcodeText.text.toString()
         val submitBarcode: Button = findViewById(R.id.submitBarcode)
+        val scanner : Button = findViewById(R.id.scanButton)
+        scanner.setOnClickListener {
+            val i = Intent(this,BarcodeScanner::class.java)
+            startActivity(i)
+        }
         submitBarcode.setOnClickListener {
             if (barcode.isEmpty()) {
                 Toast.makeText(this, "Empty barcode field", Toast.LENGTH_SHORT).show()
             } else {
-                val apiUrl = "https://lam21.modron.network/products?barcode="
-                println(accessToken)
-                val client = OkHttpClient()
-                val request = Request.Builder()
-                    .addHeader("Authorization", "Bearer $accessToken")
-                    .url(apiUrl + barcode)
-                    .build()
-                client.newCall(request).enqueue(object : Callback {
-                    override fun onFailure(call: Call, e: IOException) {
-                        Log.d("Error", "failed to execute")
-                    }
-
-                    override fun onResponse(call: Call, response: Response) {
-                        val responseBody = response.body?.string()
-                        val gson = GsonBuilder().create()
-                        val products = gson.fromJson(responseBody, Product::class.java)
-                        if (response.isSuccessful) {
-                            if (products.products.size > 0) {
-                                val i = Intent(this@GetProducts, ProductResult::class.java)
-                                i.putExtra("productName", products.products[0].name)
-                                i.putExtra("productDescription", products.products[0].description)
-                                startActivity(i)
-                            } else {
-                                runOnUiThread {
-                                    Toast.makeText(
-                                        this@GetProducts,
-                                        "No products found",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                }
-                            }
-
-                        }
-                    }
-
-                })
+                //println(accessToken)
+                val httpCalls = HTTPcalls()
+                httpCalls.GETPRODUCTS(barcode,this@GetProducts)
             }
         }
-
+runOnUiThread {  }
 
     }
 }
 
-class Product(val products: ArrayList<ProductData>)
-class ProductData(val name: String, val description: String)
