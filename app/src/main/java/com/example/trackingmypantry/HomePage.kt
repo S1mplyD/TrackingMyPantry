9cc40dca37b49dce5881ca3672cfb25779932682
S1mplyD <lucabennati2k16@gmail.com>
2021-08-03 11:00:35 +0200

    removed unused dependencies and intent extras

--- app/src/main/java/com/example/trackingmypantry/HomePage.kt
+++ app/src/main/java/com/example/trackingmypantry/HomePage.kt
@@ -1,24 +1,19 @@
 package com.example.trackingmypantry
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import okhttp3.*
-import java.io.IOException
+import androidx.appcompat.app.AppCompatActivity
 
 class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
-        val accessToken = intent.getStringExtra("accessToken").toString()
         //val btn = findViewById<Button>(R.id.localproductsButton)
         //btn.setOnClickListener { val i = Intent(this, ) }
         val getProducts: Button = findViewById(R.id.getproductsButton)
         getProducts.setOnClickListener {
             val i = Intent(this,GetProducts::class.java)
-            i.putExtra("accessToken",accessToken)
             startActivity(i)
         }
     }
