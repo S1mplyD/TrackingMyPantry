b3506c589fb89e7b19c7b3be13aa5cd44808ea3e
S1mplyD <lucabennati2k16@gmail.com>
2021-08-05 17:51:44 +0200

    changed getProducts function name

--- app/src/main/java/com/example/trackingmypantry/GetProducts.kt
+++ app/src/main/java/com/example/trackingmypantry/GetProducts.kt
@@ -1,26 +1,19 @@
 package com.example.trackingmypantry
 
-import android.content.ClipDescription
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import com.google.gson.GsonBuilder
 import okhttp3.*
-import java.io.IOException
-import com.example.trackingmypantry.HTTPcalls
-import com.example.trackingmypantry.LoginPage.Companion.accessToken
 
 class GetProducts : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_products)
         val barcodeText: EditText = findViewById(R.id.barcode)
-        val barcode = barcodeText.text.toString()
+        val barcode = barcodeText.text
         val submitBarcode: Button = findViewById(R.id.submitBarcode)
         val scanner : Button = findViewById(R.id.scanButton)
         scanner.setOnClickListener {
@@ -33,10 +26,9 @@ class GetProducts : AppCompatActivity() {
             } else {
                 //println(accessToken)
                 val httpCalls = HTTPcalls()
-                httpCalls.GETPRODUCTS(barcode,this@GetProducts)
+                httpCalls.getProducts(barcode.toString(),this@GetProducts)
             }
         }
-runOnUiThread {  }
 
     }
 }
