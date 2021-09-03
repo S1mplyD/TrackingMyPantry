86577834f70a31c2510e9030c883b9df6fc4088b
S1mplyD <lucabennati2k16@gmail.com>
2021-09-03 11:37:40 +0200

    setted dataDiScadenza, dataDiAcquisto, categorie and nuneroProdotti to nullable

--- app/src/main/java/com/example/trackingmypantry/database/Product.kt
+++ app/src/main/java/com/example/trackingmypantry/database/Product.kt
@@ -2,8 +2,6 @@ package com.example.trackingmypantry.database
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import androidx.room.TypeConverters
-import java.text.SimpleDateFormat
 import java.util.*
 
 @Entity(tableName = "products_table")
@@ -11,8 +9,8 @@ data class Product(
     val nome: String,
     val descrizione: String,
     @PrimaryKey val barcode: String,
-    val dataDiScadenza: Date,
-    val dataDiAcquisto: Date,
-    val categoria: String,
-    val numeroProdotti: Int
+    val dataDiScadenza: Date?,
+    val dataDiAcquisto: Date?,
+    val categoria: String?,
+    val numeroProdotti: Int?
 )
