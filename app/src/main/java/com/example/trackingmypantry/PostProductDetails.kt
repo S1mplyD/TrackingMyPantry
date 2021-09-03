e4283b337e4504de6eec8f64d86b8b7c7deca007
S1mplyD <lucabennati2k16@gmail.com>
2021-09-03 11:37:02 +0200

    Added date picker and insertion to local database

--- app/src/main/java/com/example/trackingmypantry/PostProductDetails.kt
+++ app/src/main/java/com/example/trackingmypantry/PostProductDetails.kt
@@ -1,5 +1,6 @@
 package com.example.trackingmypantry
 
+import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -7,29 +8,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.DatePicker
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.trackingmypantry.database.Product
+import com.example.trackingmypantry.database.ProductViewModel
+import org.w3c.dom.Text
+import java.util.*
 
 class PostProductDetails(private val token: String, private val barcode: String) :
     DialogFragment(), DatePickerDialog.OnDateSetListener {
+    private lateinit var mViewModel: ProductViewModel
+
+
     @RequiresApi(Build.VERSION_CODES.N)
     var day = 0
     var month = 0
     var year = 0
 
-    var savedDay = 0
-    var savedMonth = 0
-    var savedYear = 0
+    var startDay = 0
+    var startMonth = 0
+    var startYear = 0
+
+    var endDay = 0
+    var endMonth = 0
+    var endYear = 0
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val rootView: View =
             inflater.inflate(R.layout.activity_post_product_details, container, false)
         val nameDetails: EditText = rootView.findViewById(R.id.nameDetails)
@@ -37,9 +49,38 @@ class PostProductDetails(private val token: String, private val barcode: String)
         val submitButton: Button = rootView.findViewById(R.id.submitDetails)
         val name = nameDetails.text
         val description = descriptionDetails.text
-
         rootView.findViewById<Button>(R.id.setBuyDate).setOnClickListener {
-            DatePickerDialog(rootView.context,this,year,month,day).show()
+            getToday()
+            DatePickerDialog(
+                rootView.context,
+                DatePickerDialog.OnDateSetListener { view, year, month, day ->
+                    startYear = year
+                    startMonth = month
+                    startDay = day
+                    rootView.findViewById<TextView>(R.id.buyDate).text = "$day/${month + 1}/$year"
+                },
+                year,
+                month,
+                day
+            ).show()
+
+        }
+
+        rootView.findViewById<Button>(R.id.setExpirationDate).setOnClickListener {
+            getToday()
+            DatePickerDialog(
+                rootView.context,
+                DatePickerDialog.OnDateSetListener { view, year, month, day ->
+                    endYear = year
+                    endMonth = month
+                    endDay = day
+                    rootView.findViewById<TextView>(R.id.expirationDate).text =
+                        "$day/${month + 1}/$year"
+                },
+                year,
+                month,
+                day
+            ).show()
         }
 
         submitButton.setOnClickListener {
@@ -53,14 +94,29 @@ class PostProductDetails(private val token: String, private val barcode: String)
                     barcode,
                     context
                 )
+                val cal = Calendar.getInstance()
+                cal.clear()
+                cal.set(startYear,startMonth,startDay)
+                val dataDiScadenza = cal.time
+                cal.clear()
+                cal.set(endYear,endMonth,endDay)
+                val dataDiAcquisto = cal.time
+                val product = Product(name.toString(),description.toString(),barcode,dataDiScadenza,dataDiAcquisto,null,null)
+                mViewModel = ViewModelProvider(this).get(ProductViewModel::class.java)
+                mViewModel.insert(product)
             }
         }
         return rootView
     }
 
-    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-        savedDay = dayOfMonth
-        savedMonth = month
-        savedYear = year
+
+    private fun getToday() {
+        val cal: Calendar = Calendar.getInstance()
+        day = cal.get(Calendar.DAY_OF_MONTH)
+        month = cal.get(Calendar.MONTH)
+        year = cal.get(Calendar.YEAR)
     }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {}
+
 }
