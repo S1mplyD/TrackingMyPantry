e939a6e06700fb4de0cc4499c05337e23dace42b
S1mplyD <lucabennati2k16@gmail.com>
2021-09-06 11:45:34 +0200

    added buy date and expiration date to local product card

--- app/src/main/java/com/example/trackingmypantry/LocalProductsAdapter.kt
+++ app/src/main/java/com/example/trackingmypantry/LocalProductsAdapter.kt
@@ -1,13 +1,19 @@
 package com.example.trackingmypantry
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.example.trackingmypantry.database.Product
+import com.example.trackingmypantry.database.ProductViewModel
 
-class LocalProductsAdapter : RecyclerView.Adapter<CustomViewHolderLocal>() {
+class LocalProductsAdapter(private val mviewmodel: ProductViewModel) :
+    RecyclerView.Adapter<CustomViewHolderLocal>() {
 
     private var productList = emptyList<Product>()
 
@@ -17,16 +23,46 @@ class LocalProductsAdapter : RecyclerView.Adapter<CustomViewHolderLocal>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolderLocal {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val cellForRow = layoutInflater.inflate(R.layout.product_card, parent, false)
+        val cellForRow = layoutInflater.inflate(R.layout.product_card_local, parent, false)
         return CustomViewHolderLocal(cellForRow)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: CustomViewHolderLocal, position: Int) {
         val currentItem = productList[position]
         holder.productName.text = currentItem.nome
         holder.productDescription.text = currentItem.descrizione
+        if (!(currentItem.dataDiAcquisto.toString().length < 28 || currentItem.dataDiScadenza.toString().length < 28)) {
+            holder.productBuyDate.text =
+                "${holder.itemView.findViewById<TextView>(R.id.buyDateCard).resources.getString(R.string.buy_date)} " + currentItem.dataDiAcquisto.toString()
+                    .substring(0, 10) + " " + currentItem.dataDiAcquisto.toString()
+                    .substring(24, 28)
+            holder.productExpirationDate.text =
+                "${holder.itemView.findViewById<TextView>(R.id.expirationDateCard).resources.getString(R.string.expiration_date)} " + currentItem.dataDiScadenza.toString()
+                    .substring(0, 10) + " " + currentItem.dataDiScadenza.toString()
+                    .substring(24, 28)
+        } else {
+            holder.productBuyDate.visibility = View.GONE
+            holder.productExpirationDate.visibility = View.GONE
+        }
+
+        holder.itemView.setOnClickListener {
+            if (holder.itemView.findViewById<ConstraintLayout>(R.id.expandableLocal).isVisible) {
+                holder.itemView.findViewById<ConstraintLayout>(R.id.expandableLocal).visibility =
+                    View.GONE
+            } else {
+                holder.itemView.findViewById<ConstraintLayout>(R.id.expandableLocal).visibility =
+                    View.VISIBLE
+            }
+        }
+        holder.itemView.findViewById<Button>(R.id.deleteButton).setOnClickListener {
+            mviewmodel.delete(productList[position])
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position, productList.size)
+        }
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     fun setData(product: List<Product>) {
         this.productList = product
         notifyDataSetChanged()
@@ -38,4 +74,7 @@ class LocalProductsAdapter : RecyclerView.Adapter<CustomViewHolderLocal>() {
 class CustomViewHolderLocal(view: View) : RecyclerView.ViewHolder(view) {
     val productName: TextView = view.findViewById(R.id.productName)
     val productDescription: TextView = view.findViewById(R.id.productDescription)
+    val productBuyDate: TextView = view.findViewById(R.id.buyDateCard)
+    val productExpirationDate: TextView = view.findViewById(R.id.expirationDateCard)
+
 }
