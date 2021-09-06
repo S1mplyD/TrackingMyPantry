8b1050841fe29140292b9ba01a25a6ffab6a1a02
S1mplyD <lucabennati2k16@gmail.com>
2021-09-06 11:44:13 +0200

    Added searchbar to local products

--- app/src/main/java/com/example/trackingmypantry/LocalProducts.kt
+++ app/src/main/java/com/example/trackingmypantry/LocalProducts.kt
@@ -2,24 +2,65 @@ package com.example.trackingmypantry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.trackingmypantry.database.ProductViewModel
 
-class LocalProducts : AppCompatActivity() {
+class LocalProducts : AppCompatActivity(), SearchView.OnQueryTextListener {
+
+    private lateinit var mviewmodel: ProductViewModel
+    private lateinit var adapter: LocalProductsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_local_products)
-        val mviewmodel: ProductViewModel
+
+        mviewmodel = ViewModelProvider(this).get(ProductViewModel::class.java)
         val recview: RecyclerView = findViewById(R.id.localProductsRecView)
-        val adapter = LocalProductsAdapter()
+        adapter = LocalProductsAdapter(mviewmodel)
         recview.layoutManager = LinearLayoutManager(this)
         recview.adapter = adapter
-        mviewmodel = ViewModelProvider(this).get(ProductViewModel::class.java)
+
         mviewmodel.allProducts.observe(this, Observer { product ->
             adapter.setData(product)
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_search, menu)
+
+        val search = menu?.findItem(R.id.search_menu)
+        val searchView = search?.actionView as? SearchView
+        searchView?.isSubmitButtonEnabled = true
+        searchView?.setOnQueryTextListener(this)
+
+        return true
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        if(query != null){
+            searchDatabase(query)
+        }
+        return true
+    }
+
+    override fun onQueryTextChange(query: String?): Boolean {
+        if(query != null){
+            searchDatabase(query)
+        }
+        return true
+    }
+
+    private fun searchDatabase(query: String) {
+        val searchQuery = "%$query%"
+        mviewmodel.searchDatabase(searchQuery).observe(this, { list ->
+            list.let {
+                adapter.setData(it)
+            }
         })
     }
 
