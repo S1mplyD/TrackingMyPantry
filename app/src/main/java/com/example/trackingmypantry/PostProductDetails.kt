8906b66fd14491e68bf43b4c4d5c6086ac66411b
S1mplyD <lucabennati2k16@gmail.com>
2022-01-03 11:57:15 +0100

    Added category in PostProductDetails.kt

--- app/src/main/java/com/example/trackingmypantry/PostProductDetails.kt
+++ app/src/main/java/com/example/trackingmypantry/PostProductDetails.kt
@@ -3,9 +3,7 @@ package com.example.trackingmypantry
 import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,9 +11,8 @@ import android.widget.*
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.trackingmypantry.database.Product
-import com.example.trackingmypantry.database.ProductViewModel
-import org.w3c.dom.Text
+import com.example.trackingmypantry.room_database.Product
+import com.example.trackingmypantry.room_database.ProductViewModel
 import java.util.*
 
 class PostProductDetails(private val token: String?, private val barcode: String) :
@@ -45,12 +42,24 @@ class PostProductDetails(private val token: String?, private val barcode: String
     ): View? {
         val rootView: View =
             inflater.inflate(R.layout.activity_post_product_details, container, false)
+        var itemCategory = ""
         val nameDetails: EditText = rootView.findViewById(R.id.nameDetails)
         val descriptionDetails: EditText = rootView.findViewById(R.id.descriptionDetails)
         val nameField = nameDetails.text
         val descriptionField = descriptionDetails.text
 
         val submitButton: Button = rootView.findViewById(R.id.submitDetails)
+
+        val category : Spinner = rootView.findViewById(R.id.categoriesSelector)
+        category.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                itemCategory = adapterView?.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                Toast.makeText(rootView.context,"Nothing selected",Toast.LENGTH_SHORT).show()
+            }
+        }
 
         rootView.findViewById<Button>(R.id.setBuyDate).setOnClickListener {
             getToday()
@@ -87,7 +96,7 @@ class PostProductDetails(private val token: String?, private val barcode: String
         }
 
         submitButton.setOnClickListener {
-            if (nameField.isNullOrBlank() || descriptionField.isNullOrBlank()) {
+            if (nameField.isNullOrBlank() || descriptionField.isNullOrBlank() || itemCategory.isNullOrBlank()) {
                 Toast.makeText(context, "Empty field", Toast.LENGTH_SHORT).show()
             } else {
                 HTTPcalls().postProductsDetails(
@@ -104,7 +113,7 @@ class PostProductDetails(private val token: String?, private val barcode: String
                 cal.clear()
                 cal.set(endYear,endMonth,endDay,0,0,0)
                 val dataDiScadenza = cal.time
-                val product = Product(nameField.toString(),descriptionField.toString(),barcode,dataDiScadenza,dataDiAcquisto,null,null)
+                val product = Product(nameField.toString(),descriptionField.toString(),barcode,dataDiScadenza,dataDiAcquisto,itemCategory,null)
                 mViewModel = ViewModelProvider(this).get(ProductViewModel::class.java)
                 mViewModel.insert(product)
                 dismiss()
