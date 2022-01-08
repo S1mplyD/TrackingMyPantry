a38e9073a013d69bbbbfe1dc2d3a499e6c653d7f
S1mplyD <lucabennati2k16@gmail.com>
2022-01-08 12:14:21 +0100

    Add comments to LoginPage class

--- app/src/main/java/com/example/trackingmypantry/LoginPage.kt
+++ app/src/main/java/com/example/trackingmypantry/LoginPage.kt
@@ -1,30 +1,38 @@
 package com.example.trackingmypantry
 
+import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import android.widget.*
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class LoginPage : AppCompatActivity() {
+    //Companion object creato per salvare l'access token e renderlo disponibile in qualsiasi punto del codice
     companion object {
         var accessToken = ""
     }
 
+    //Shared preferences per salvare il token su file e lo stato del "Ricordami"
     lateinit var sharedPreferences: SharedPreferences
     var isRemembered = false
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
-        sharedPreferences = getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
 
+        //Controllo se durante il login l'utente ha scelto di essere ricordato
+        sharedPreferences = getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
         isRemembered = sharedPreferences.getBoolean("CHECKBOX", false)
 
+        //Se l'utente ha scelto di essere ricordato recupero il suo access token dalle shared preference
         if (isRemembered) {
-
-            accessToken = sharedPreferences.getString("ACCESS_TOKEN","").toString()
+            accessToken = sharedPreferences.getString("ACCESS_TOKEN", "").toString()
             val i = Intent(this, HomePage::class.java)
             startActivity(i)
             finish()
@@ -34,6 +42,8 @@ class LoginPage : AppCompatActivity() {
         val loginPassword: EditText = findViewById(R.id.loginPassword)
         val loginButton = findViewById<Button>(R.id.loginButton)
         val rememberMe: CheckBox = findViewById(R.id.rememberUser)
+
+        //Controllo che i campi di log in non siano vuoti e faccio una chiamata http a login
         loginButton.setOnClickListener {
             if (loginMail.text.isNullOrBlank() || loginPassword.text.isNullOrBlank()) {
                 Toast.makeText(this, "Empty field", Toast.LENGTH_SHORT).show()
@@ -48,12 +58,15 @@ class LoginPage : AppCompatActivity() {
             }
         }
 
+        //Link che permette a chi non è registrato di essere reindirizzato alla pagina apposita
         val registerLink: TextView = findViewById(R.id.linkToRegister)
         registerLink.setOnClickListener {
-            val i = Intent(this, RegisterPage::class.java) //go to login page
+            val i = Intent(this, RegisterPage::class.java)
             startActivity(i)
         }
+
     }
+
 }
 
 class AccessToken(val accessToken: String)
